6b4e5ae3bc42d2d04c217e122344f05896a8965d
Dale Puckett <dale.puckett@att.net>
2016-09-19 18:28:10 -0500

    Finished first version of Emoji Dictionary

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,76 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Dale Puckett on 9/19/16.
+//  Copyright © 2016 k0hyd. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    
+    @IBOutlet var definitionLabel: UILabel!
+    @IBOutlet var emojiLabel: UILabel!
+    var emoji = "NO EMOJI"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        emojiLabel.text = emoji
+        
+        if emoji == "✈️" {
+        definitionLabel.text = "An airplane to fly away"
+        }
+        if emoji == "😜" {
+            definitionLabel.text = "Winking faced with stuck out tounge"
+        }
+        if emoji == "👲" {
+            definitionLabel.text = "A dude with sunglasses"
+        }
+        if emoji == "🍾" {
+            definitionLabel.text = "Champagne for a celebration"
+        }
+        if emoji == "❤" {
+            definitionLabel.text = "A warm heart"
+        }
+        if emoji == "😉" {
+            definitionLabel.text = "Winking face"
+        }
+        if emoji == "🎈" {
+            definitionLabel.text = "A balloon for a party"
+        }
+        if emoji == "🚌" {
+            definitionLabel.text = "Bus trip"
+        }
+        if emoji == "😈" {
+            definitionLabel.text = "Smiling face with horns"
+        }
+        if emoji == "😍" {
+            definitionLabel.text = "Smiling face with heart shaped eyes"
+        }
+        if emoji == "🚗" {
+            definitionLabel.text = "A car for your road trip"
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -35,10 +35,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "MoveSeque", sender: "✈️")
+        tableView.deselectRow(at: indexPath, animated: true)
+        let emoji = emojis[indexPath.row]
+        performSegue(withIdentifier: "MoveSeque", sender: emoji)
     }
     
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let defVC = segue.destination as! DefinitionViewController
+        defVC.emoji = sender as! String
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
